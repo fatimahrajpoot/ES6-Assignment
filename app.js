2e4b30f2293f2f2bf0781ec7a49615510ee00ee7
Fatimah Rajput <fatimarajput750@gmail.com>
2024-11-22 12:23:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js
+
+// Import functions from mathOperations.js
+import { add, subtract, multiply, divide } from './mathOperations.js';
+
+// Perform calculations and log results
+console.log("Addition:", add(10, 5)); // 15
+console.log("Subtraction:", subtract(10, 5)); // 5
+console.log("Multiplication:", multiply(10, 5)); // 50
+console.log("Division:", divide(10, 5)); // 2
